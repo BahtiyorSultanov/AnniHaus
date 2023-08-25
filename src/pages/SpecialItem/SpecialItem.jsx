c16a32d961cf158cc8a61abdd88c8623d7f266a3
BahtiyorSultanov <sbahtiyour@gmail.com>
2023-08-25 12:59:30 +0500

    swiper

--- src/pages/SpecialItem/SpecialItem.jsx
+++ src/pages/SpecialItem/SpecialItem.jsx
@@ -15,10 +15,11 @@ function SpecialItem() {
       const { data } = await axios.get(`http://localhost:3000/cards/${id}`);
       setCard(data);
     },
-    async () => {
+    setTimeout(
+      async () => {
       const { data } = await axios.get(`http://localhost:3000/cards`);
       setCards(data);
-    }
+    }, 1)
   );
   const [swiperRef, setSwiperRef] = useState(null);
   const appendNumber = useRef(500);
@@ -43,7 +44,7 @@ function SpecialItem() {
   return (
     <div className="">
       <div className="flex flex-col gap-[30px] items-start">
-        <div className="">
+        <div className="flex flex-col gap-[30px] items-start">
           <img src={card.prevue} className="rounded-[8px]" />
           <span className="px-[32px] rounded-[48px] text-[#242424] text-[12px] py-[8px] border-[1px] border-[#EFCF2C]">
             Специальное предложение
@@ -58,9 +59,10 @@ function SpecialItem() {
             </h2>
           </div>
         </div>
-        <div className="">
+        <div className="flex flex-col items-start gap-[30px]">
+          <h1 className="text-[40px] text-[#3B3B3B]">В акции участвуют следующие товары</h1>
           <Swiper
-            className="ml-[-40px]"
+            className="ml-[-140px]"
             modules={[Virtual, Navigation, Pagination]}
             onSwiper={setSwiperRef}
             slidesPerView={3}
@@ -73,24 +75,32 @@ function SpecialItem() {
             virtual
           >
             {cards &&
-              cards.map((item) => {
+              cards.map((item, key) => {
                 return (
                   item.kind == "sale" && (
                     <SwiperSlide className="bg-transparent flex flex-col items-center">
-                      <div className="">
+                      <div className="" key={key}>
                         <div className="w-[260px] h-[260px] rounded-[8px] border-[2px]">
                           <img
                             className="w-full h-full rounded-[14px] mb-[20px]"
                             src={item.img}
                           />
                         </div>
-                        <h1 className="absolute w-[252px] text-left">
+                        <h1 className="relative w-[252px] text-left mt-[10px]">
                           {item.title}
                         </h1>
-                        <div className="flex items-center justify-start gap-[20px]">
-                          {item.color &&
-                            item.color.map((imem) => {
-                              return <div className=""></div>;
+                        <h1 className="relative w-[252px] text-left">
+                          {item.price}
+                        </h1>
+                        <div className="flex items-center justify-start gap-[20px] mt-[10px]">
+                          {item.colors &&
+                            item.colors.map((item) => {
+                              return (
+                                <div
+                                  className={`w-[16px] h-[16px] rounded-[50%]`}
+                                  style={{ background: item }}
+                                ></div>
+                              );
                             })}
                         </div>
                       </div>
